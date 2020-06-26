f23e0cbd7405c40d5f93d988230f15ecb5957436
Vorkytaka <vorkytaka@gmail.com>
2020-06-26 21:49:38 +0300

    • Fix IDEntity, remove mapper as a parameter

--- lib/entity/IDEntity.dart
+++ lib/entity/IDEntity.dart
@@ -1,11 +1,10 @@
 class IDEntity<T> {
   final int id;
   final T entity;
-  final Function(T) mapper;
 
-  IDEntity(this.id, this.entity, this.mapper);
+  IDEntity(this.id, this.entity);
 
-  Map<String, dynamic> toMap() {
+  Map<String, dynamic> toMap(Function(T) mapper) {
     final map = {"id": id};
     map.addAll(mapper(entity));
     return map;
