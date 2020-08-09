c183242e4ef7906e0ace840ff7d53ebd103535ee
Vorkytaka <vorkytaka@gmail.com>
2020-08-09 16:15:14 +0300

    • Add `Credentials` tests

---
+++ test/entity/credentials.dart
@@ -0,0 +1,16 @@
+import 'package:antassistant/entity/credentials.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("Credentials", () {
+    test("Requires a non-null username", () {
+      expect(() => Credentials(username: null, password: "password"),
+          throwsAssertionError);
+    });
+
+    test("Requires a non-null password", () {
+      expect(() => Credentials(username: "username", password: null),
+          throwsAssertionError);
+    });
+  });
+}
