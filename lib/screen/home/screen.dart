ffedb603f771d4937c34f31cb65e5d344bf22cfb
Vorkytaka <vorkytaka@gmail.com>
2020-08-30 04:32:47 +0300

    • Add update button to the main screen

--- lib/screen/home/screen.dart
+++ lib/screen/home/screen.dart
@@ -1,7 +1,9 @@
 import 'package:antassistant/bloc/data/bloc.dart';
+import 'package:antassistant/bloc/data/event.dart';
 import 'package:antassistant/bloc/data/state.dart';
 import 'package:antassistant/screen/detailed/provider.dart';
 import 'package:antassistant/widget/user_data_card_widget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,36 +13,54 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: null,
       body: SafeArea(
-        child: BlocBuilder<UserDataBloc, UserDataState>(
-          builder: (BuildContext context, UserDataState state) {
-            if (state is DataIsLoading) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (state is DataLoaded) {
-              return ListView.builder(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 16,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.refresh),
+                  onPressed: () {
+                    BlocProvider.of<UserDataBloc>(context).add(AskForUpdate());
+                  },
                 ),
-                itemCount: state.data.length,
-                itemBuilder: (context, i) => Hero(
-                  tag: "user_data_${state.data[i].accountId}",
-                  child: UserDataCardWidget(
-                    data: state.data[i],
-                    onTap: () async {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (BuildContext context) {
-                          return DetailedScreenProvider(data: state.data[i]);
-                        },
-                      ));
-                    },
-                  ),
-                ),
-              );
-            } else {
-              return Container();
-            }
-          },
+              ],
+            ),
+            BlocBuilder<UserDataBloc, UserDataState>(
+              builder: (BuildContext context, UserDataState state) {
+                if (state is DataIsLoading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state is DataLoaded) {
+                  return Expanded(
+                    child: ListView.builder(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 16,
+                      ),
+                      itemCount: state.data.length,
+                      itemBuilder: (context, i) => Hero(
+                        tag: "user_data_${state.data[i].accountId}",
+                        child: UserDataCardWidget(
+                          data: state.data[i],
+                          onTap: () async {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (BuildContext context) {
+                                return DetailedScreenProvider(
+                                    data: state.data[i]);
+                              },
+                            ));
+                          },
+                        ),
+                      ),
+                    ),
+                  );
+                } else {
+                  return Container();
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
