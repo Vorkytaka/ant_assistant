c2b961711f27627402082b9b6b4eab59a2233654
Vorkytaka <vorkytaka@gmail.com>
2020-08-30 04:05:32 +0300

    • Use new `UserDataCardWidget` on `Authenticated` screen

--- lib/screen/home/widget/authenticated.dart
+++ lib/screen/home/widget/authenticated.dart
@@ -1,8 +1,7 @@
-import 'package:animations/animations.dart';
 import 'package:antassistant/bloc/data/bloc.dart';
 import 'package:antassistant/bloc/data/state.dart';
-import 'package:antassistant/entity/user_data.dart';
-import 'package:antassistant/screen/detailed_user_data/provider.dart';
+import 'package:antassistant/screen/detailed/provider.dart';
+import 'package:antassistant/widget/user_data_card_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -17,76 +16,25 @@ class AuthenticatedWidget extends StatelessWidget {
             child: CircularProgressIndicator(),
           );
         } else if (state is DataLoaded) {
-          return ListView.separated(
-            itemCount: state.data.length,
-            separatorBuilder: (context, i) => Container(
-              color: Colors.grey,
-              height: 1,
+          return ListView.builder(
+            padding: EdgeInsets.symmetric(
+              horizontal: 16,
             ),
-            itemBuilder: (context, i) => _buildItem(state.data[i], context),
+            itemCount: state.data.length,
+            itemBuilder: (context, i) => UserDataCardWidget(
+              data: state.data[i],
+              onTap: () async {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return DetailedScreenProvider(data: state.data[i]);
+                  },
+                ));
+              },
+            ),
           );
         } else {
           return Container();
         }
-      },
-    );
-  }
-
-  Widget _buildItem(UserData data, BuildContext context) {
-    return OpenContainer(
-      transitionType: ContainerTransitionType.fade,
-      transitionDuration: Duration(
-        milliseconds: 300,
-      ),
-      tappable: false,
-      openColor: Theme.of(context).cardColor,
-      openBuilder: (context, anim) {
-        return DetailedUserDataScreenProvider(
-          credentialsId: data.credentialsId,
-        );
-      },
-      closedShape: const RoundedRectangleBorder(),
-      closedColor: Theme.of(context).cardColor,
-      closedBuilder: (context, openContainer) {
-        return Card(
-          shape: const RoundedRectangleBorder(),
-          margin: EdgeInsets.zero,
-          child: InkWell(
-            onTap: openContainer,
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                vertical: 16,
-                horizontal: 24,
-              ),
-              child: Row(
-                children: <Widget>[
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        data.accountName,
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 6,
-                      ),
-                      Text(
-                        "Баланс: ${data.balance} ₽",
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
-        );
       },
     );
   }
