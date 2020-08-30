cefcf837701e8deaada10d367e7e06b5db798eb8
Vorkytaka <vorkytaka@gmail.com>
2020-08-30 03:53:22 +0300

    • Base of new `UserDataCardWidget`

---
+++ lib/widget/user_data_card_widget.dart
@@ -0,0 +1,80 @@
+import 'package:antassistant/entity/user_data.dart';
+import 'package:flutter/material.dart';
+
+class UserDataCardWidget extends StatelessWidget {
+  final UserData data;
+
+  const UserDataCardWidget({Key key, @required this.data}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3,
+      child: InkWell(
+        onTap: () {},
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 24,
+            vertical: 16,
+          ),
+          child: Column(
+            children: [
+              Text(
+                "Имя пользователя",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                "${data.accountName}",
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          "Баланс",
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "${data.balance}",
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          "Дней осталось",
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "${data.daysLeft()}",
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
