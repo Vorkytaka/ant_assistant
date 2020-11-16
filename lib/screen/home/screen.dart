505447e15e00f1f9b4244d183cb1eee637a22ac4
Vorkytaka <vorkytaka@gmail.com>
2020-11-17 02:33:40 +0300

    • Remove all old design of main & detailed screens

--- lib/screen/home/screen.dart
+++ lib/screen/home/screen.dart
@@ -1,8 +1,6 @@
 import 'package:antassistant/bloc/data/bloc.dart';
 import 'package:antassistant/bloc/data/event.dart';
 import 'package:antassistant/bloc/data/state.dart';
-import 'package:antassistant/screen/detailed/provider.dart';
-import 'package:antassistant/widget/user_data_card_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -38,20 +36,7 @@ class HomeScreen extends StatelessWidget {
                         horizontal: 16,
                       ),
                       itemCount: state.data.length,
-                      itemBuilder: (context, i) => Hero(
-                        tag: "user_data_${state.data[i].accountId}",
-                        child: UserDataCardWidget(
-                          data: state.data[i],
-                          onTap: () async {
-                            Navigator.of(context).push(MaterialPageRoute(
-                              builder: (BuildContext context) {
-                                return DetailedScreenProvider(
-                                    data: state.data[i]);
-                              },
-                            ));
-                          },
-                        ),
-                      ),
+                      itemBuilder: (context, i) => Text("${state.data[i].accountName}"),
                     );
                   } else {
                     return Container();
