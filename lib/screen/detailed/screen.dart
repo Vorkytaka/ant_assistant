65ad0f1f4e9280015bb9239c0811f438271266fc
Vorkytaka <vorkytaka@gmail.com>
2021-01-08 03:12:39 +0300

    • Move body of `DetailedUserDataScreen` to another widget

--- lib/screen/detailed/screen.dart
+++ lib/screen/detailed/screen.dart
@@ -29,87 +29,9 @@ class DetailedUserDataScreen extends StatelessWidget {
         verticalDirection: VerticalDirection.up,
         children: [
           Flexible(
-            child: ListView(
+            child: DetailedUserDataBody(
+              data: data,
               controller: controller,
-              physics: const ScrollPhysics(),
-              padding: const EdgeInsets.only(
-                bottom: 16,
-              ),
-              children: [
-                SizedBox(width: 16),
-                _buildInfoCard(
-                  context,
-                  Icons.account_balance_wallet,
-                  "Баланс",
-                  data.statusInfo.balance.toString(),
-                ),
-                _buildInfoCard(
-                  context,
-                  Icons.calendar_today_outlined,
-                  "Дней осталось",
-                  data.daysLeft.toString(),
-                ),
-                _buildInfoCard(
-                  context,
-                  Icons.volunteer_activism,
-                  "Кредит доверия",
-                  data.statusInfo.credit.toString(),
-                ),
-                _buildInfoCard(
-                  context,
-                  Icons.credit_card_rounded,
-                  "Код плательщика",
-                  data.accountId,
-                ),
-                _buildInfoCard(
-                  context,
-                  Icons.wb_sunny_rounded,
-                  "Состояние",
-                  data.statusInfo.status,
-                ),
-                _buildInfoCard(
-                  context,
-                  Icons.label_important,
-                  "Название тарифа",
-                  data.tariffInfo.tariffName,
-                ),
-                _buildInfoCard(
-                  context,
-                  Icons.attach_money_outlined,
-                  "Цена за месяц",
-                  data.tariffInfo.pricePerMonth.toString(),
-                ),
-                _buildInfoCard(
-                  context,
-                  Icons.attach_money_outlined,
-                  "Цена за день",
-                  data.tariffInfo.pricePerDay.toString(),
-                ),
-                _buildInfoCard(
-                  context,
-                  Icons.download_rounded,
-                  "Скачано за текущий месяц",
-                  data.statusInfo.downloaded.toString(),
-                ),
-                _buildInfoCard(
-                  context,
-                  Icons.arrow_forward,
-                  "Скорость загрузки",
-                  data.tariffInfo.downloadSpeed,
-                ),
-                _buildInfoCard(
-                  context,
-                  Icons.arrow_back,
-                  "Скорость отдачи",
-                  data.tariffInfo.uploadSpeed,
-                ),
-                _buildInfoCard(
-                  context,
-                  Icons.dynamic_feed,
-                  "Ваш DynDNS",
-                  data.dynDns,
-                ),
-              ],
             ),
           ),
           AppBar(
@@ -140,6 +62,137 @@ class DetailedUserDataScreen extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildRemoveAccountDialog(BuildContext context, UserData data) {
+    return AlertDialog(
+      title: Text("Удаление аккаунта"),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Вы уверены, что хотите удалить аккаунт ${data.accountName}?"),
+          SizedBox(height: 8),
+          Text("Это действие нельзя отменить"),
+        ],
+      ),
+      actions: [
+        FlatButton(
+          child: Text("Нет"),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        RaisedButton(
+          child: Text("Да"),
+          onPressed: () {
+            BlocProvider.of<UserDataBloc>(context).add(
+                UserDataEvent.DeleteUser(credentialsId: data.credentialsId));
+            BlocProvider.of<AuthBloc>(context)
+                .add(AuthBlocEvent.DeleteUser(id: data.credentialsId));
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class DetailedUserDataBody extends StatelessWidget {
+  final UserData data;
+  final ScrollController controller;
+
+  const DetailedUserDataBody({
+    Key key,
+    @required this.data,
+    this.controller,
+  })  : assert(data != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      controller: controller,
+      physics: const ScrollPhysics(),
+      padding: const EdgeInsets.only(
+        bottom: 16,
+      ),
+      children: [
+        SizedBox(width: 16),
+        _buildInfoCard(
+          context,
+          Icons.account_balance_wallet,
+          "Баланс",
+          data.statusInfo.balance.toString(),
+        ),
+        _buildInfoCard(
+          context,
+          Icons.calendar_today_outlined,
+          "Дней осталось",
+          data.daysLeft.toString(),
+        ),
+        _buildInfoCard(
+          context,
+          Icons.volunteer_activism,
+          "Кредит доверия",
+          data.statusInfo.credit.toString(),
+        ),
+        _buildInfoCard(
+          context,
+          Icons.credit_card_rounded,
+          "Код плательщика",
+          data.accountId,
+        ),
+        _buildInfoCard(
+          context,
+          Icons.wb_sunny_rounded,
+          "Состояние",
+          data.statusInfo.status,
+        ),
+        _buildInfoCard(
+          context,
+          Icons.label_important,
+          "Название тарифа",
+          data.tariffInfo.tariffName,
+        ),
+        _buildInfoCard(
+          context,
+          Icons.attach_money_outlined,
+          "Цена за месяц",
+          data.tariffInfo.pricePerMonth.toString(),
+        ),
+        _buildInfoCard(
+          context,
+          Icons.attach_money_outlined,
+          "Цена за день",
+          data.tariffInfo.pricePerDay.toString(),
+        ),
+        _buildInfoCard(
+          context,
+          Icons.download_rounded,
+          "Скачано за текущий месяц",
+          data.statusInfo.downloaded.toString(),
+        ),
+        _buildInfoCard(
+          context,
+          Icons.arrow_forward,
+          "Скорость загрузки",
+          data.tariffInfo.downloadSpeed,
+        ),
+        _buildInfoCard(
+          context,
+          Icons.arrow_back,
+          "Скорость отдачи",
+          data.tariffInfo.uploadSpeed,
+        ),
+        _buildInfoCard(
+          context,
+          Icons.dynamic_feed,
+          "Ваш DynDNS",
+          data.dynDns,
+        ),
+      ],
     );
   }
 
@@ -184,39 +237,6 @@ class DetailedUserDataScreen extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-
-  Widget _buildRemoveAccountDialog(BuildContext context, UserData data) {
-    return AlertDialog(
-      title: Text("Удаление аккаунта"),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Вы уверены, что хотите удалить аккаунт ${data.accountName}?"),
-          SizedBox(height: 8),
-          Text("Это действие нельзя отменить"),
-        ],
-      ),
-      actions: [
-        FlatButton(
-          child: Text("Нет"),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        RaisedButton(
-          child: Text("Да"),
-          onPressed: () {
-            BlocProvider.of<UserDataBloc>(context).add(
-                UserDataEvent.DeleteUser(credentialsId: data.credentialsId));
-            BlocProvider.of<AuthBloc>(context)
-                .add(AuthBlocEvent.DeleteUser(id: data.credentialsId));
-            Navigator.of(context).pop();
-          },
-        ),
-      ],
     );
   }
 }
