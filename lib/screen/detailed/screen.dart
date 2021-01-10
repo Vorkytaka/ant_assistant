0576f3ba1ca579b1d0f420760c1f1140dc251de5
Vorkytaka <vorkytaka@gmail.com>
2021-01-11 02:58:12 +0300

    • Handle selection of initial page

--- lib/screen/detailed/screen.dart
+++ lib/screen/detailed/screen.dart
@@ -52,13 +52,32 @@ class DetailedUserDataScreen extends StatelessWidget {
 }
 
 class DetailedUserPagesScreen extends StatelessWidget {
+  /// [credentialsId] uses for select initial page
+  /// if it's [null] then initial page will be the first page
+  final int credentialsId;
+
+  const DetailedUserPagesScreen({
+    Key key,
+    this.credentialsId,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: BlocBuilder<UserDataBloc, UserDataState>(
         builder: (context, state) {
           if (state is DataLoaded) {
+            int initialPage = 0;
+            if (credentialsId != null) {
+              for (int i = 0; i < state.data.length; i++) {
+                if (state.data[i].credentialsId == credentialsId) {
+                  initialPage = i;
+                  break;
+                }
+              }
+            }
             return PageView.builder(
+              controller: PageController(initialPage: initialPage),
               itemCount: state.data.length,
               itemBuilder: (context, i) {
                 return DetailedUserDataScreen(data: state.data[i]);
