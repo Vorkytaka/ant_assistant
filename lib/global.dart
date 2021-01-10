8330e75a9da588f800f6073645efd766af11a6d5
Vorkytaka <vorkytaka@gmail.com>
2021-01-11 02:28:30 +0300

    • Move creating of delete user dialog to the global.dart

--- lib/global.dart
+++ lib/global.dart
@@ -1,3 +1,11 @@
+import 'package:antassistant/bloc/auth/bloc.dart';
+import 'package:antassistant/bloc/auth/event.dart' as AuthBlocEvent;
+import 'package:antassistant/bloc/data/bloc.dart';
+import 'package:antassistant/bloc/data/event.dart' as UserDataEvent;
+import 'package:antassistant/entity/user_data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 class URIs {
   const URIs._();
 
@@ -6,4 +14,44 @@ class URIs {
     scheme: "tel",
     path: "+7 495 940-92-11",
   );
+}
+
+class Dialogs {
+  const Dialogs._();
+
+  static Widget _buildRemoveAccountDialog(
+    BuildContext context,
+    UserData data,
+  ) {
+    return AlertDialog(
+      title: Text("Удаление аккаунта"),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Вы уверены, что хотите удалить аккаунт ${data.accountName}?"),
+          SizedBox(height: 8),
+          Text("Это действие нельзя отменить"),
+        ],
+      ),
+      actions: [
+        FlatButton(
+          child: Text("Нет"),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        RaisedButton(
+          child: Text("Да"),
+          onPressed: () {
+            BlocProvider.of<UserDataBloc>(context).add(
+                UserDataEvent.DeleteUser(credentialsId: data.credentialsId));
+            BlocProvider.of<AuthBloc>(context)
+                .add(AuthBlocEvent.DeleteUser(id: data.credentialsId));
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
 }
--- lib/screen/detailed/screen.dart
+++ lib/screen/detailed/screen.dart
@@ -1,13 +1,8 @@
-import 'package:antassistant/bloc/auth/bloc.dart';
-import 'package:antassistant/bloc/auth/event.dart' as AuthBlocEvent;
-import 'package:antassistant/bloc/data/bloc.dart';
-import 'package:antassistant/bloc/data/event.dart' as UserDataEvent;
 import 'package:antassistant/entity/user_data.dart';
 import 'package:antassistant/screen/detailed/widget/body.dart';
 import 'package:antassistant/screen/detailed/widget/header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DetailedUserDataScreen extends StatelessWidget {
   final UserData data;
@@ -49,39 +44,6 @@ class DetailedUserDataScreen extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-
-  Widget _buildRemoveAccountDialog(BuildContext context, UserData data) {
-    return AlertDialog(
-      title: Text("Удаление аккаунта"),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Вы уверены, что хотите удалить аккаунт ${data.accountName}?"),
-          SizedBox(height: 8),
-          Text("Это действие нельзя отменить"),
-        ],
-      ),
-      actions: [
-        FlatButton(
-          child: Text("Нет"),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        RaisedButton(
-          child: Text("Да"),
-          onPressed: () {
-            BlocProvider.of<UserDataBloc>(context).add(
-                UserDataEvent.DeleteUser(credentialsId: data.credentialsId));
-            BlocProvider.of<AuthBloc>(context)
-                .add(AuthBlocEvent.DeleteUser(id: data.credentialsId));
-            Navigator.of(context).pop();
-          },
-        ),
-      ],
     );
   }
 }
