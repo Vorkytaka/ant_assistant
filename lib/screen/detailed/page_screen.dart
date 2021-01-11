62ec6c5e4f92de02d624a28d2be85c2038e57e71
Vorkytaka <vorkytaka@gmail.com>
2021-01-11 03:00:31 +0300

    • Move `DetailedUserDataPageViewScreen` to another file

---
+++ lib/screen/detailed/page_screen.dart
@@ -0,0 +1,48 @@
+import 'package:antassistant/bloc/data/bloc.dart';
+import 'package:antassistant/bloc/data/state.dart';
+import 'package:antassistant/screen/detailed/screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class DetailedUserDataPageViewScreen extends StatelessWidget {
+  static final String ROUTE = "DetailedUserDataPageViewScreen";
+
+  /// [credentialsId] uses for select initial page
+  /// if it's [null] then initial page will be the first page
+  final int credentialsId;
+
+  const DetailedUserDataPageViewScreen({
+    Key key,
+    this.credentialsId,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocBuilder<UserDataBloc, UserDataState>(
+        builder: (context, state) {
+          if (state is DataLoaded) {
+            int initialPage = 0;
+            if (credentialsId != null) {
+              for (int i = 0; i < state.data.length; i++) {
+                if (state.data[i].credentialsId == credentialsId) {
+                  initialPage = i;
+                  break;
+                }
+              }
+            }
+            return PageView.builder(
+              controller: PageController(initialPage: initialPage),
+              itemCount: state.data.length,
+              itemBuilder: (context, i) {
+                return DetailedUserDataScreen(data: state.data[i]);
+              },
+            );
+          }
+
+          return Container();
+        },
+      ),
+    );
+  }
+}
--- lib/screen/detailed/screen.dart
+++ lib/screen/detailed/screen.dart
@@ -1,11 +1,8 @@
-import 'package:antassistant/bloc/data/bloc.dart';
-import 'package:antassistant/bloc/data/state.dart';
 import 'package:antassistant/entity/user_data.dart';
 import 'package:antassistant/screen/detailed/widget/body.dart';
 import 'package:antassistant/screen/detailed/widget/header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DetailedUserDataScreen extends StatelessWidget {
   final UserData data;
@@ -46,47 +43,6 @@ class DetailedUserDataScreen extends StatelessWidget {
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class DetailedUserPagesScreen extends StatelessWidget {
-  /// [credentialsId] uses for select initial page
-  /// if it's [null] then initial page will be the first page
-  final int credentialsId;
-
-  const DetailedUserPagesScreen({
-    Key key,
-    this.credentialsId,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocBuilder<UserDataBloc, UserDataState>(
-        builder: (context, state) {
-          if (state is DataLoaded) {
-            int initialPage = 0;
-            if (credentialsId != null) {
-              for (int i = 0; i < state.data.length; i++) {
-                if (state.data[i].credentialsId == credentialsId) {
-                  initialPage = i;
-                  break;
-                }
-              }
-            }
-            return PageView.builder(
-              controller: PageController(initialPage: initialPage),
-              itemCount: state.data.length,
-              itemBuilder: (context, i) {
-                return DetailedUserDataScreen(data: state.data[i]);
-              },
-            );
-          }
-
-          return Container();
-        },
       ),
     );
   }
