e1d2c0eb2c08d2a11c3d502c7104b484f59c63da
Vorkytaka <vorkytaka@gmail.com>
2021-01-11 03:25:13 +0300

    • Base for new `UserDataState`

--- lib/bloc/data/state.dart
+++ lib/bloc/data/state.dart
@@ -15,3 +15,30 @@ class DataLoaded extends UserDataState {
   // todo: another state for empty data?
   const DataLoaded({@required this.data}) : assert(data != null);
 }
+
+class NewUserDataState {
+  final UserDataStateStatus status;
+  final List<UserData> data;
+
+  NewUserDataState({
+    @required this.status,
+    this.data,
+  }) : assert(status != null);
+
+  NewUserDataState copyWith({
+    UserDataStateStatus status,
+    List<UserData> data,
+  }) {
+    return NewUserDataState(
+      status: status ?? this.status,
+      data: data ?? this.data,
+    );
+  }
+}
+
+enum UserDataStateStatus {
+  INITIAL,
+  LOADING,
+  SUCCESS,
+  FAILURE,
+}
